4fb9ac745a2fe6b03e90e424931581f6e8ba003d
Jan Smrcina <jsmrcina@gmail.com>
2022-04-22 00:05:26 -0700

    Fixes

--- aliases.ps1
+++ aliases.ps1
@@ -1,2 +1,24 @@
+# Aliases
 Function CdToGit { Set-Location -Path $global:gitFolderPath}
 Set-Alias -Name cdgit -Value CdToGit
+
+# Add items to path
+Function Add-PathVariable {
+    param (
+        [string]$addPath
+    )
+
+    if (Test-Path $addPath)
+    {
+        $regexAddPath = [regex]::Escape($addPath)
+        $arrPath = $env:Path -split ';' | Where-Object {$_ -notMatch "^$regexAddPath\\?"}
+        $env:Path = ($arrPath + $addPath) -join ';'
+    }
+    else
+    {
+        Throw "'$addPath' is not a valid path."
+    }
+}
+
+## Sublime
+Add-PathVariable "C:\Program Files\Sublime Text 3"
--- source.ps1
+++ source.ps1
@@ -7,9 +7,9 @@ param(
 # Make available to other scripts
 $global:gitFolderPath = $gitFolderPath
 
-Get-ChildItem $developerFolderPath | % {
+Get-ChildItem $developerFolderPath -filter *.ps1 | % {
     if($_.Name -ne "source.ps1")
-	{
+    {
         . $_.FullName
     }
 }
