e01fe739dc1a0af8a3eda7e6e6b34b3d7764eda7
Jan Smrcina <6693580+jsmrcina@users.noreply.github.com>
2023-04-03 02:03:28 -0700

    Add ownership check to checkingit

--- checkingit.ps1
+++ checkingit.ps1
@@ -1,4 +1,36 @@
-function global:checkingit([string]$repourl)
+function global:checkingit([string]$owner)
 {
-    gci | ForEach-Object { if(-not (Test-Path "$_\.git")) { Write-Host "$_ is not in Git" } }
+    gci | ForEach-Object {
+        if(-not (Test-Path "$_\.git"))
+        {
+            Write-Host "$_ is not in Git"
+        }
+        else
+        {
+            foreach($line in Get-Content "$_\.git\config")
+            {
+                if($line.Trim().StartsWith("url = "))
+                {
+                    # HTTPS
+                    if($line.Contains("https"))
+                    {
+                        if(-not ($line.Contains($owner)))
+                        {
+                            Write-Host "$_ is not owned by $owner"
+                        }
+                    }
+                    # SSH
+                    elseif($line.Contains("git@github.com"))
+                    {
+                        if(-not ($line.Contains($owner)))
+                        {
+                            Write-Host "$_ is not owned by $owner"
+                        }
+                    }
+                }
+            }
+
+            return
+        }
+    }
 }
