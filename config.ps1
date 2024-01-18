9bfc3510d38c15d996234c93c9a86cd459ca78f6
Jan Smrcina <6693580+jsmrcina@users.noreply.github.com>
2024-01-18 14:02:16 -0800

    Add Z module installation

--- config.ps1
+++ config.ps1
@@ -1,2 +1,9 @@
 ## Make sure git is using Windows OpenSSH
 git config --global core.sshCommand C:/Windows/System32/OpenSSH/ssh.exe
+
+## Make sure powershell Z is installed
+if (-not (Get-Module -ListAvailable -Name z))
+{
+    Write-Host "Installing Z module"
+    Install-Module -Name z  
+}
