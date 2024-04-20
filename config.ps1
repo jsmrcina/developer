f51661cd01976075d616ba96cabcdba233e9f39c
Jan Smrcina <6693580+jsmrcina@users.noreply.github.com>
2024-04-19 18:42:39 -0700

    Add some aliasing

--- config.ps1
+++ config.ps1
@@ -7,3 +7,8 @@ if (-not (Get-Module -ListAvailable -Name z))
     Write-Host "Installing Z module"
     Install-Module -Name z  
 }
+
+git config --global alias.s status
+git config --global alias.c commit
+git config --global alias.ch checkout
+git config --global alias.a add
