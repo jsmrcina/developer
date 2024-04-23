e4343e7050b40515cde202978c127ab9fe7cf36c
Jan Smrcina <6693580+jsmrcina@users.noreply.github.com>
2024-04-23 13:13:30 -0700

    Clean up log

--- aliases.ps1
+++ aliases.ps1
@@ -13,5 +13,5 @@ git config --global alias.a add
 git config --global alias.rb rebase
 git config --global alias.p push
 git config --global alias.f fetch
-git config --global alias.l10 'log --pretty=format:"%h`t`t%s" -n10'
+git config --global alias.l10 'log --pretty=format:"%<|(10)%Cgreen%h%Creset%Cred%<(15,trunc)%an%Creset%<(50,mtrunc)%s" -n10'
 git config --global alias.br 'branch -vv'
