43a05992f3dddba477f8fab031acf3176ec7aa5d
Jan Smrcina <6693580+jsmrcina@users.noreply.github.com>
2024-04-23 15:00:08 -0700

    Alias branch a bit differently

--- aliases.ps1
+++ aliases.ps1
@@ -14,4 +14,4 @@ git config --global alias.rb rebase
 git config --global alias.p push
 git config --global alias.f fetch
 git config --global alias.l10 'log --pretty=format:"%<|(10)%Cgreen%h%Creset%Cred%<(15,trunc)%an%Creset%<(50,mtrunc)%s" -n10'
-git config --global alias.br 'branch -vv'
+git config --global alias.br 'branch -vv --all'
