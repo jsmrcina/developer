294f3c316d229e484862423a33f54760fb072acc
Jan Smrcina <6693580+jsmrcina@users.noreply.github.com>
2024-04-23 12:59:39 -0700

    Config

--- aliases.ps1
+++ aliases.ps1
@@ -4,3 +4,14 @@ Set-Alias -Name cdgit -Value CdToGit
 
 ## Global variables
 $global:developer_dir = Split-Path $MyInvocation.MyCommand.Path
+
+## Git aliases
+git config --global alias.s status
+git config --global alias.c commit
+git config --global alias.ch checkout
+git config --global alias.a add
+git config --global alias.rb rebase
+git config --global alias.p push
+git config --global alias.f fetch
+git config --global alias.l10 'log --pretty=format:"%h`t`t%s" -n10'
+git config --global alias.br 'branch -vv'
--- config.ps1
+++ config.ps1
@@ -8,12 +8,7 @@ if (-not (Get-Module -ListAvailable -Name z))
     Install-Module -Name z  
 }
 
-git config --global alias.s status
-git config --global alias.c commit
-git config --global alias.ch checkout
-git config --global alias.a add
-git config --global alias.rb rebase
-git config --global alias.p push
-git config --global alias.f fetch
-git config --global alias.l10 'log --pretty=format:"%h`t`t%s" -n10'
-git config --global alias.br 'branch -vv'
+## Git Configuration
+git config --global push.autoSetupRemote true
+git config --global core.editor gvim
+git config --global rebase.autosquash true
