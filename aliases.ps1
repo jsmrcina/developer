4bf8e3d6fa08b421fc31a8b4166341c5edbb36fb
Jan Smrcina <6693580+jsmrcina@users.noreply.github.com>
2024-06-13 16:31:00 -0700

    Update some aliases to be more useful

--- aliases.ps1
+++ aliases.ps1
@@ -8,10 +8,16 @@ $global:developer_dir = Split-Path $MyInvocation.MyCommand.Path
 ## Git aliases
 git config --global alias.s status
 git config --global alias.c commit
-git config --global alias.ch checkout
+git config --global alias.cm '!f() { git commit -m $1; }; f'
+git config --global alias.cf '!f() { git commit --fixup $1 -m $2; }; f'
+git config --global alias.chm '!f() { git checkout -b "$1" origin/main --no-track; }; f'
 git config --global alias.a add
+git config --global alias.aa '!f() { git add *; }; f'
 git config --global alias.rb rebase
+git config --global alias.rom '!f() { git fetch; git rebase origin/main; }; f'
 git config --global alias.p push
 git config --global alias.f fetch
-git config --global alias.l10 'log --pretty=format:"%<|(10)%Cgreen%h%Creset%Cred%<(15,trunc)%an%Creset%<(50,mtrunc)%s" -n10'
+git config --global alias.fp 'fetch --prune'
+git config --global alias.l '!f() { git log --pretty=format:"%<|(10)%Cgreen%h%Creset%Cred%<(15,trunc)%an%Creset%<(50,mtrunc)%s" -n$1; }; f'
+git config --global alias.la 'log --pretty=format:"%<|(10)%Cgreen%h%Creset%Cred%<(15,trunc)%an%Creset%<(50,mtrunc)%s"'
 git config --global alias.br 'branch -vv --all'
