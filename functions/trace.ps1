18f8e3e46a7edfe251f88ebaefb88b7c9eff944e
Jan Smrcina <6693580+jsmrcina@users.noreply.github.com>
2025-02-27 16:04:41 -0800

    Improve trace script

--- functions/trace.ps1
+++ functions/trace.ps1
@@ -4,11 +4,11 @@ function global:trace($guid = "ce5fa4ea-ab00-5402-8b76-9f76ac858fb5", $file = "C
     $guidWithStack = "$guid:::'stack'"
 
     xperf -start trace -on $guidWithStack -f $file
-    xperf -on PROC_THREAD+LOADER+PROFILE -stackwalk Profile
+    xperf -on PROC_THREAD+LOADER -f $kernelFile
     Write-Host -ForegroundColor Yellow "Repro your issue now..."
     pause
+    xperf -stop
     xperf -stop trace
-    xperf -d $kernelFile
     xperf -merge $file $kernelFile $outFile
 
     Write-Host -ForegroundColor Green "Output file is at $outFile"
