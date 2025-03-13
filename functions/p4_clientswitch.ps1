8165019d05f3ed25cda997f1423e9ac858da3f45
Jan Smrcina <6693580+jsmrcina@users.noreply.github.com>
2025-03-13 15:42:31 -0700

    Combine server and client p4 switch into single command

--- functions/p4_clientswitch.ps1
+++
@@ -1,4 +0,0 @@
-function global:p4_clientswitch($name)
-{
-    p4 set P4CLIENT=$name
-}
---
+++ functions/p4_switch.ps1
@@ -0,0 +1,15 @@
+function global:p4_switch($address, $user, $clientDefinition)
+{
+    p4 set P4PORT=$address
+    p4 set P4USER=$user
+    p4 set P4CLIENT=$clientDefinition
+}
+
+function global:p4_switchf($file)
+{
+    $config = Get-Content $file | ConvertFrom-Json
+
+    p4 set P4PORT = $config.address
+    p4 set P4USER = $config.user
+    p4 set P4CLIENT = $config.client
+}
